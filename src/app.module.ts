98ec9ce59ee0b40e5a5e74a79cf527efecde348a
milon27 <mdjahidulislammilon@gmail.com>
2023-03-27 06:14:28 +0600

    added and setup prisma + databse module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
+import { DatabaseModule } from "./database/database.module"
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        DatabaseModule,
     ],
     controllers: [],
     providers: [],
